4c1e16fc104bd4c3bdc1d221a035b885f26cce61
volodavkaa <volodavkaa@gmail.com>
2024-12-07 15:39:37 +0200

    1 Зробив вивід списку потів. 2 До фото можна додати url 3 вивід списку постів адаптований під різні розміри екранів

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -3,5 +3,5 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'created_at', 'updated_at')  
-    prepopulated_fields = {'slug': ('title',)} 
+    list_display = ('title', 'slug', 'created_at', 'updated_at', 'photo_url')  
+    prepopulated_fields = {'slug': ('title',)}
--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,11 +1,18 @@
 from django.db import models
+from django.utils.text import slugify
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
     content = models.TextField()
-    slug = models.SlugField(unique=True, blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)  
-    updated_at = models.DateTimeField(auto_now=True) 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    photo_url = models.URLField(max_length=500, blank=True, null=True)  # Поле для URL фото
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
